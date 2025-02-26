75555edaaa47bd79f9cf32e7b2ec82dd72a1e12c
trevans24 <trevans24@gmail.com>
2025-02-26 11:42:09 -0700

    auto fit column in grid to fix responsive issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import styles from "./page.module.css"
 import LaunchList from "./components/LaunchList"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <LaunchList />
-      </div>
+    <main>
+      <LaunchList />
     </main>
   )
 }
